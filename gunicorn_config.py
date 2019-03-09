bf0101ca938efbcdaca057a818beb4f42e35ae79
Victor Nate Graf <nategraf1@gmail.com>
2019-03-09 22:35:42 +0000

    update configurations to serve 2.0

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -8,14 +8,14 @@ import base64
 bind='0.0.0.0:8000'
 
 # Worker settings
-worker_class=os.eviron.get('WORKER_CLASS', 'gevent')
-worker=int(os.eviron.get('WORKERS', 2*multiprocessing.cpu_count()))
+worker_class=os.environ.get('WORKER_CLASS', 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker')
+worker=int(os.environ.get('WORKERS', 2*multiprocessing.cpu_count()))
 
 # Logging settings
 loglevel='info'
 log_folder = os.environ['LOG_FOLDER']
-accesslog=os.environ.get('ACCESS_LOG', os.path.join(log_folder, 'access.log'))
-errorlog=os.environ.get('ERROR_LOG', os.path.join(log_folder, 'error.log'))
+accesslog=os.environ.get('ACCESS_LOG', '-')
+errorlog=os.environ.get('ERROR_LOG', '-')
 
 if not os.environ.get('USE_RELOAD') or os.environ.get('USE_RELOAD').lower() == 'true':
     # Reload on code changes. Useful for development
